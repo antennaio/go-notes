5c34c8a276f67538b14d4eeaece0fcec9bfcd563
kazik <antenna@antenna.io>
2019-08-25 21:05:52 +0200

    Custom implementation of Authenticator middleware

--- api/auth/tokenauth.go
+++ api/auth/tokenauth.go
@@ -1,14 +1,17 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 	"os"
 	"sync"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/jwtauth"
+	"github.com/go-chi/render"
 
 	"github.com/antennaio/goapi/api/user"
+	"github.com/antennaio/goapi/lib/error"
 )
 
 var auth *tokenAuth
@@ -28,7 +31,23 @@ func (a *tokenAuth) Verifier() func(http.Handler) http.Handler {
 }
 
 func (a *tokenAuth) Authenticator() func(http.Handler) http.Handler {
-	return jwtauth.Authenticator
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token, _, err := jwtauth.FromContext(r.Context())
+
+			if err != nil {
+				render.Render(w, r, error.Unauthorized(errors.New("Invalid token.")))
+				return
+			}
+
+			if token == nil || !token.Valid {
+				render.Render(w, r, error.Unauthorized(errors.New("Invalid token.")))
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
 
 // Use the singleton pattern to initialise the TokenAuth service
