280bb7455713e844ad2fe6254f5b479fada06bd2
kazik <antenna@antenna.io>
2019-10-02 11:56:12 +0200

    Delete note - add test

--- tests/api_note_test.go
+++ tests/api_note_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -65,8 +66,10 @@ func TestGetNotes(t *testing.T) {
 }
 
 func TestCreateNote(t *testing.T) {
+	defer generator.truncateNotes()
+
 	body, errBody := json.Marshal(map[string]string{
-		"title": "New note",
+		"title":   "New note",
 		"content": "Content",
 	})
 	if errBody != nil {
@@ -87,6 +90,27 @@ func TestCreateNote(t *testing.T) {
 		t.Error(err)
 	}
 	assert.Equal(t, 1, count, "Expected 1 note, got %v", count)
+}
+
+func TestDeleteNote(t *testing.T) {
+	notes := generator.generateNotes(u, 1)
+	defer generator.truncateNotes()
+
+	id := strconv.Itoa(notes[0].Id)
+	request, errRequest := http.NewRequest("DELETE", "/v1/note/"+id, nil)
+	if errRequest != nil {
+		t.Error(errRequest)
+	}
+	request.Header.Add("Authorization", "BEARER "+token)
+	response := recordResponse(a.Router, request)
+
+	verifyResponseCode(t, http.StatusOK, response.Code)
+
+	count, err := a.Pg.Model((*note.Note)(nil)).Count()
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, 0, count, "Expected 0 notes, got %v", count)
 }
 
 func TestMain(m *testing.M) {
